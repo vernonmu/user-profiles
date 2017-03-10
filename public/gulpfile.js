cb4470e9b5731057cb84a04564bbf168d4d8f708
Vernon <me@example.com>
2017-03-10 14:22:17 -0600

    added gulp and gulp magic

---
+++ public/gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+
+var path = {
+  scripts: ['public/app.js', 'public/**/*.js', '!public/build.js'],
+  css: ['public/css/*.css']
+}
+
+gulp.task('default', ['build', 'minify'], function() {
+  console.log('go gulp, go!');
+});
+
+gulp.task('build', function() {
+  gulp.src(path.scripts)
+    .pipe(concat('build.js'))
+    .pipe(gulp.dest('public'))
+});
+
+gulp.task('minify', function() {
+  gulp.src(path.css)
+    .pipe(concat(style.css))
+    .pipe(gulp.dest('public'))
+});
